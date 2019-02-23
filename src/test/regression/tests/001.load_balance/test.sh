caeb9eb16ef2c11b24ec3898d60820da7d9e9a5c
Dmitri Moore <demisxbar@gmail.com>
2019-02-22 19:28:34 -0800

    Add version 4.0.2

---
+++ src/test/regression/tests/001.load_balance/test.sh
@@ -0,0 +1,182 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------
+# test script for load balancing.
+#
+source $TESTLIBS
+TESTDIR=testdir
+PSQL=$PGBIN/psql
+
+for mode in s r
+do
+	rm -fr $TESTDIR
+	mkdir $TESTDIR
+	cd $TESTDIR
+
+# create test environment
+	echo -n "creating test environment..."
+	$PGPOOL_SETUP -m $mode -n 2 || exit 1
+	echo "done."
+
+	source ./bashrc.ports
+
+	echo "backend_weight0 = 0" >> etc/pgpool.conf
+	echo "backend_weight1 = 1" >> etc/pgpool.conf
+	echo "black_function_list = 'f1'" >> etc/pgpool.conf
+
+	./startall
+
+	export PGPORT=$PGPOOL_PORT
+
+	wait_for_pgpool_startup
+
+	$PSQL test <<EOF
+CREATE TABLE t1(i INTEGER);
+CREATE TABLE t2(i INTEGER);
+CREATE FUNCTION f1(INTEGER) returns INTEGER AS 'SELECT \$1' LANGUAGE SQL;
+SELECT * FROM t1;		-- this load balances
+SELECT f1(1);		-- this does not load balance
+EOF
+
+# check if simple load balance worked
+	fgrep "SELECT * FROM t1;" log/pgpool.log |grep "DB node id: 1">/dev/null 2>&1
+	if [ $? != 0 ];then
+	# expected result not found
+		echo fail: select is sent to zero-weight node.
+		./shutdownall
+		exit 1
+	fi
+	echo ok: load balance works.
+
+# check if black function list worked
+	fgrep "SELECT f1(1);" log/pgpool.log |grep "DB node id: 0">/dev/null 2>&1
+	if [ $? != 0 ];then
+	# expected result not found
+		echo fail: black function is sent to node 1.
+		./shutdownall
+		exit 1
+	fi
+	echo ok: black function list works.
+
+	echo "white_function_list = 'f1'" >> etc/pgpool.conf
+	echo "black_function_list = ''" >> etc/pgpool.conf
+
+	./pgpool_reload
+	sleep 1
+
+	$PSQL test <<EOF
+SELECT f1(1);		-- this does load balance
+EOF
+
+# check if white function list worked
+	fgrep "SELECT f1(1);" log/pgpool.log |grep "DB node id: 1">/dev/null 2>&1
+	if [ $? != 0 ];then
+	# expected result not found
+		echo fail: white function is sent to zero-weight node.
+		./shutdownall
+		exit 1
+	fi
+	echo ok: white function list works.
+
+# check if black query pattern list worked
+	./shutdownall
+	echo "black_query_pattern_list = 'SELECT \'a\'\;;SELECT 1\;;SELECT \'\;\'\;;SELECT \* FROM t1\;;^.*t2.*\;$;^.*f1.*$'" >> etc/pgpool.conf
+	./startall
+	wait_for_pgpool_startup
+
+queries=`cat << EOF
+SELECT * FROM t1;
+SELECT 'a';
+SELECT 1;
+SELECT ';';
+SELECT * FROM t2;
+SELECT f1(1);
+EOF
+`
+	echo "$queries" | while read query; do
+		$PSQL test -c "$query"
+
+		# If master-slave mode, all queries are sent to primary node only.
+		# If query match both black_query_pattern_list and white_function_list,
+		# white_function_list will be ignored, and query is sent to primary node only.
+		#
+		# If replication node, all queries are load-blanced.
+		if [[ $mode = "s" ]];then
+			node_id=0
+		else
+			node_id=1
+		fi
+		fgrep "${query}" log/pgpool.log | grep "DB node id: "`echo $node_id` > /dev/null 2>&1
+
+		if [ $? != 0 ];then
+			# expected result not found
+			echo "fail: black query: ${query} is load-blanced."
+			./shutdownall
+			exit 1
+		fi
+	done
+
+	if [ $? -eq 1 ]; then
+		exit 1
+	fi
+	echo ok: black query pattern list works.
+
+
+# in replication mode if load_balance_mode = off, SELECT query inside
+# an explicit transaction should be sent to master only.
+	if [ $mode = "r" ];then
+		./shutdownall
+		echo "load_balance_mode = off" >> etc/pgpool.conf
+		./startall
+		wait_for_pgpool_startup
+
+		$PSQL test <<EOF
+BEGIN;
+SELECT 1;
+END;
+EOF
+
+		ok=0
+		fgrep "SELECT 1;" log/pgpool.log |grep "DB node id: 0">/dev/null 2>&1
+		if [ $? = 0 ];then
+			fgrep "SELECT 1;" log/pgpool.log |grep "DB node id: 1">/dev/null 2>&1		
+			if [ $? != 0 ];then
+			# the SELECT should not be executed on node 1
+				echo ok: select is sent to only master when not load-blanced.
+				ok=1
+			fi
+		# the SELECT should be executed on node 0
+		fi
+
+# in replication mode if load_balance_mode = off, SELECT query
+# including writing function should be sent to all the nodes.
+# per [pgpool-general: 2221].
+		echo "black_function_list = 'f1'" >> etc/pgpool.conf
+		echo "white_function_list = ''" >> etc/pgpool.conf
+		./pgpool_reload
+		sleep 1
+		$PSQL test <<EOF
+SELECT f1(2);		-- this should be sent to all the nodes
+EOF
+		fgrep "SELECT f1(2);" log/pgpool.log |grep "DB node id: 0">/dev/null 2>&1
+		if [ $? = 0 ];then
+			fgrep "SELECT f1(2);" log/pgpool.log |grep "DB node id: 1">/dev/null 2>&1		
+			if [ $? = 0 ];then
+			# the SELECT should be executed on node 0 & 1
+				echo ok: black function is sent to all nodes.
+				ok=`expr $ok + 1`
+			fi
+		# the SELECT should be executed on node 0
+		fi
+
+		if [ $ok != 2 ];then
+		    exit 1;
+		fi
+	fi
+
+	./shutdownall
+
+	cd ..
+
+done
+
+exit 0
---
+++ src/tools/pgproto/send.c
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2017-2018	Tatsuo Ishii
+ *
+ * Permission to use, copy, modify, and distribute this software and
+ * its documentation for any purpose and without fee is hereby
+ * granted, provided that the above copyright notice appear in all
+ * copies and that both that copyright notice and this permission
+ * notice appear in supporting documentation, and that the name of the
+ * author not be used in advertising or publicity pertaining to
+ * distribution of the software without specific, written prior
+ * permission. The author makes no representations about the
+ * suitability of this software for any purpose.  It is provided "as
+ * is" without express or implied warranty.
+ */
+
+#include "../../include/config.h"
+#include "pgproto/pgproto.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <getopt.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include "pgproto/fe_memutils.h"
+#include <libpq-fe.h>
+#include "pgproto/read.h"
+#include "pgproto/send.h"
+
+/*
+ * Send a character to the connection.
+ */
+void
+send_char(char c, PGconn *conn)
+{
+	write(PQsocket(conn), &c, 1);
+}
+
+/*
+ * Send a 4-byte integer to the connection.
+ */
+void
+send_int(int intval, PGconn *conn)
+{
+	int			l = htonl(intval);
+
+	write(PQsocket(conn), &l, sizeof(l));
+}
+
+/*
+ * Send a 2-byte integer to the connection.
+ */
+void
+send_int16(short shortval, PGconn *conn)
+{
+	short		s = htons(shortval);
+
+	write(PQsocket(conn), &s, sizeof(s));
+}
+
+/*
+ * Send a string to the connection. buf must be NULL terminated.
+ */
+void
+send_string(char *buf, PGconn *conn)
+{
+	write(PQsocket(conn), buf, strlen(buf) + 1);
+}
+
+/*
+ * Send byte to the connection.
+ */
+void
+send_byte(char *buf, int len, PGconn *conn)
+{
+	write(PQsocket(conn), buf, len);
+}
